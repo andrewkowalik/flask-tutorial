f830b77fad7ccc9dcb0508ce4a7539d37551fb12
Drew Kowalik <dk@epantry.com>
2014-04-23 08:10:49 -0700

    update models for flask-security

--- app/models.py
+++ app/models.py
@@ -1,12 +1,25 @@
 from app import db
+from flask.ext.security import Security, SQLAlchemyUserDatastore, \
+    UserMixin, RoleMixin, login_required
 
 ROLE_USER = 0
 ROLE_ADMIN = 1
 
-class User(db.Model):
+roles_users = db.Table('roles_users',
+    db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+    db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, index = True)
     email = db.Column(db.String(120), unique=True, index = True)
+    roles = db.relationship('Role', secondary=roles_users,
+        backref=db.backref('users', lazy='dynamic'))
 
     def __init__(self, username, email):
         self.username = username
